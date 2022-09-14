0bc95909920a73f71af031255e456e5d2a5644cc
Elise Giroux <elise.giroux@vtsd.com>
2022-09-14 13:10:56 -0400

    9-14-22 lab

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,7 +1,15 @@
 public class Lab02vst {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
+        System.out.println("Lab02, 80 point version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure todays's knowledge ");
+        System.out.println("by yesterdays confusion");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
 
     }
 
